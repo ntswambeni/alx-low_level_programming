1c03b64cbb1c767f56a100072aeaf9749d7596db
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-14 16:24:07 -0700

    add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - this is the entry point for the program
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 50);
+	return (1);
+}
