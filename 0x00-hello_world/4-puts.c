196411ea1e9e31b2bacd7778f35d25032921a994
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-14 13:01:13 -0700

    add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  * main - this is the entry point of the program
+ * Return: 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
