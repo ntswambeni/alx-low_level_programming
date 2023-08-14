ca9a59428e325215b35ce3f76aef07860f1f7c68
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-14 16:32:24 -0700

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <string.h>
 
 /**
  * main - this is the entry point for the program
@@ -7,6 +8,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, strlen(message));
 	return (1);
 }
