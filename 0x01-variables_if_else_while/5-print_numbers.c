c91104c17943dac2c05809f2238e4cfc7d7b378c
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-15 16:01:13 -0700

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the program
+ * Return: 0
+ */
+int main(void)
+{
+	short i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
