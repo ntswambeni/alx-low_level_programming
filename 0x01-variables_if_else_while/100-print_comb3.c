438a3ad58c2d70b70772731d88942720f82365a8
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-15 16:55:54 -0700

    add 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the program
+ * Return: 0
+ */
+int main(void)
+{
+	short i;
+	short j;
+
+	for (i = 0; i < 9; i++)
+	{
+		j = i + 1;
+		for (j; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
