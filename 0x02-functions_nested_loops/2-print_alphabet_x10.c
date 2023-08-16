ac693be183367ef8a70170247fd28296f266a83b
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-16 15:38:17 -0700

    add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all letter of the alphabet in lowercase 10x
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
