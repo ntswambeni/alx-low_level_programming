5374271a33d63a6da1544659a4fc231212319b1b
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-17 10:55:00 -0700

    fix times result

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,18 +12,17 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			tmp = i * j;
-			if (tmp < 10)
+			if (j > 0 && tmp < 10)
 			{
 				_putchar(' ');
-				_putchar(tmp + '0');
 			}
-			else
+			else if (tmp >= 10)
 			{
-				tmp2 = tmp % 10;
-				tmp = tmp / 10;
-				_putchar(tmp + '0');
+				tmp2 = tmp / 10;
+				tmp = tmp % 10;
 				_putchar(tmp2 + '0');
 			}
+			_putchar(tmp + '0');
 			if (j < 9)
 			{
 				_putchar(',');
