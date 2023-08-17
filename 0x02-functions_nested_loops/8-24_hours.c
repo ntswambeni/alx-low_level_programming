2bd4d9c97bb65b125aae64710e89506897c54584
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-17 09:30:11 -0700

    fix betty style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@ void jack_bauer(void)
 	int i, j, k, l;
 
 	for (i = 0; i <= 2; i++)
-		for (j = 0; i == 2 ? j <= 3: j <= 9; j++)
+		for (j = 0; i == 2 ? j <= 3 : j <= 9; j++)
 			for (k = 0; k <= 5; k++)
 				for (l = 0; l <= 9; l++)
 				{
