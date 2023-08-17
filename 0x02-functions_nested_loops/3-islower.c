873e8537e040ebfe7619600fee4b481f1dba6d77
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-16 17:02:56 -0700

    add 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <regex.h>
+#include <stddef.h>
+
+/**
+ * _islower - checks if a character is lowercase or not
+ * @c - the charater to test
+ * Return: 1 if lowercase. 0 otherwise
+ */
+int _islower(int c)
+{
+	regex_t rx;
+	int response;
+	char str[2];
+
+	str[0] = c;
+	str[1] = '\0';
+
+	regcomp(&rx, "[a-z]", 0);
+	response = regexec(&rx, str, 0, NULL, 0);
+	if (response == 0)
+		return (1);
+	return (0);
+}
