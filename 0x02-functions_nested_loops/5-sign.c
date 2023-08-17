5555383a4ee511e726478fa81e53d1ccda225c99
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-16 17:34:06 -0700

    add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: The int to test
+ * Return: 1 if greater than 0. -1 if less smaller than 0. 0 if 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
