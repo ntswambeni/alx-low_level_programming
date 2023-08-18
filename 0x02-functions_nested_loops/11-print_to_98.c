0f787a6477e16f60cb1e33c7515175a46b6cc8d8
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-17 17:06:29 -0700

    add 0. Multiple mains

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,12 +6,55 @@
  */
 void print_to_98(int n)
 {
-	int i;
-	for (i = n; i <= 98; i++)
+	int i,abs, tmp1, tmp2, tmp3;
+
+	for (i = n; n <= 98 ? i <= 98 : i >= 98; n <= 98 ? i++ : i--)
 	{
-		_putchar(i + '0');
-		_putchar(',');
-		_putchar(' ');
+		if (i < 0)
+		{
+			abs = -i;
+			_putchar('-');
+		}
+		else
+		{
+			abs = i;
+		}
+		if (abs > 99)
+		{
+			tmp3 = abs % 10;
+			tmp2 = abs / 10;
+			tmp2 = tmp2 % 10;
+			tmp1 = abs / 100;
+			_putchar(tmp1 + '0');
+			_putchar(tmp2 + '0');
+		}
+		else if (abs > 9)
+		{
+			tmp2 = abs / 10;
+			tmp3 = abs % 10;
+			_putchar(tmp2 + '0');
+		}
+		else
+		{
+			tmp3 = abs;
+		}
+		_putchar(tmp3 + '0');
+		if (n < 98)
+		{
+			if (i < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		else
+		{
+			if (i > 98)
+			{
+				_putchar(',');
+				_putchar(' ');	
+			}
+		}
 	}
 	_putchar('\n');
 }
