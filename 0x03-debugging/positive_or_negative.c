f22545f8b0c678bd969ba55a41ab7ab7796f6b26
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-17 17:27:40 -0700

    fix amount of lines

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point for the program
+ * Return: 0
+ */
+int positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+	return (0);
+}
