7f84e64c1538a5dc3a45b96682b6c3a016a5809f
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-21 19:22:41 -0700

    add 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *reset_ro_98 - changes the value of a variable
+ *the pointer points to, to 98
+ @*n: the pointer of a variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef HEADER_FILES
+#define HEADER_FILES
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
