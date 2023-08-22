911208c0ee61c4a527b999c9f5431e53b2519bed
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-21 19:56:01 -0700

    add 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,13 @@
 
 /**
  * _strlen - returns the lenght of a string
- * s - pointer to the string var
+ * @s: pointer to the string var
+ * Return: lenght of string
  */
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
