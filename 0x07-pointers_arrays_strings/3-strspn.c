45fbe081560d1a149026455f46dc49a1ea497b2f
Felizardo Massimbe <f2massimbe@gmail.com>
2023-08-28 16:54:16 -0700

    add 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: prefix substring
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int numberOfBytes = 0, i, j;
+	char have;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		have = 'f';
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				have = 't';
+				break;
+			}
+		}
+
+		if (have != 't')
+			break;
+
+		numberOfBytes++;
+	}
+
+	return (numberOfBytes);
+}
