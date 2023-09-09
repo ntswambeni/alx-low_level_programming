cd97b9bfae1d5cff0c5962d25774c843ac78cf07
Felizardo Massimbe <f2massimbe@gmail.com>
2023-09-09 16:45:09 -0700

    add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ * @argc: number of arguments in the program
+ * @argv: array of strings/argument values
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+
+	return (0);
+}
