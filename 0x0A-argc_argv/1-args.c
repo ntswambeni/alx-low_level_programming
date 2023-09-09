5d73f5bf5724c120c88c56cad65693c2daaad215
Felizardo Massimbe <f2massimbe@gmail.com>
2023-09-09 16:53:19 -0700

    fix usused spelling in 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,9 @@
  * @argv: array of strings/argument values
  * Return: exit value
  */
-int main(int argc, __attribute__((unsed)) char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
