8d970d1457e04384545f56513f2fee83327105d8
Felizardo Massimbe <f2massimbe@gmail.com>
2023-09-09 17:06:40 -0700

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point for the program
+ * @argc: number of arguments in the program
+ * @argv: array of strings/argument values
+ * Return: exit value
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
