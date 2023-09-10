ba6f9f8379e8be914fbced96dc0e75447bd1a87e
Felizardo Massimbe <f2massimbe@gmail.com>
2023-09-09 18:19:50 -0700

    add 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - Entry point for the program
+ * @argc: number of arguments in the program
+ * @argv: array of strings/argument values
+ * Return: exit value
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	char *aux;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		aux = argv[i];
+		while (*aux != '\0')
+		{
+			isdigit(*aux);
+			if (!isdigit(*aux))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			aux++;
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
