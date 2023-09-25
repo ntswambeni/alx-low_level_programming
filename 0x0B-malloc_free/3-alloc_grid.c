a1dc11236029bdd1ae8e3ad4d1ded54e4f1d8f7f
Felizardo Massimbe <f2massimbe@gmail.com>
2023-09-25 16:06:24 -0700

    fix: 0 length array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * alloc_grid - returns a pointer to a two dimensional array
@@ -13,17 +14,24 @@ int **alloc_grid(int width, int height)
 
 	array = (int **)malloc(sizeof(int *) * height);
 
+	if (width == 0 || height == 0)
+		return (NULL);
+
 	if (array != NULL)
+	{
 		for (i = 0; i < height; i++)
 		{
 			array[i] = (int *)malloc(width * sizeof(int));
 			if (array[i] == NULL)
 				return (NULL);
 		}
+	}
+	else
+		return (0);
 
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			array[i][j] = 0;
 
-	return (array);
+	return (0);
 }
