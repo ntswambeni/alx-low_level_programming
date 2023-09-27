4f4555098fcd7958e4960827d73d07bca1ff2ee2
Felizardo Massimbe <f2massimbe@gmail.com>
2023-09-27 03:51:47 -0700

    remove month 1

--- 0x09-static_libraries/100-atoi.c
+++
@@ -1,8 +0,0 @@
-/**
- * atoi - does something
- * s: a string
- * Return: a char
- */
-int _atoi(char *s){
-	return 1;
-}
--- 0x09-static_libraries/9-strcpy.c
+++
@@ -1,9 +0,0 @@
-/**
- * strcpy - does something
- * @dest: destination string
- * @src: src string
- * Return: a string
- */
-char *_strcpy(char *dest, char *src){
-	return dest;
-}
