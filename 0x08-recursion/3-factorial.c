bd472957951a8b400d55cbc5a8100a0a1685d5cc
Felizardo Massimbe <f2massimbe@gmail.com>
2023-09-28 19:49:10 -0700

    fix: betty style on 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,12 +4,12 @@
  * factorial - returns factorial of a given number
  * @n: given number
  * Return: factorial
- * */
+ */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	if (n <= 1)
 		return (1);
-	return n * factorial(n - 1);
+	return (n * factorial(n - 1));
 }
