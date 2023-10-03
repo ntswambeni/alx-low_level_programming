3dd39761c144d4cc20aff674614fe866b1ebc653
Felizardo Massimbe <f2massimbe@gmail.com>
2023-10-02 17:58:44 -0700

    add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdion.h>
+
+/**
+ * main - entry point of the program
+ * @argc: number of arguments in the program
+ * @argv: array of strings/argument values
+ * Return: exit value
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
