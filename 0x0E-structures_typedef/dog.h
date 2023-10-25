4402a0f0e6b10b1aa25a01638d01d9cc98847c12
Felizardo Massimbe <f2massimbe@gmail.com>
2023-10-24 19:18:09 -0700

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILES
+#define HEADER_FILES
+
+/**
+ * struct dog - prototype of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
