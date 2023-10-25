f49e6239cb58ad7185b683e98c7b27787efb5f0a
Felizardo Massimbe <f2massimbe@gmail.com>
2023-10-25 06:11:11 -0700

    add 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,14 +25,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (namecopy == NULL)
 		return (NULL);
 	ownercopy = malloc(sizeof(owner));
-	if (ownercopy = NULL)
+	if (ownercopy == NULL)
 		return (NULL);
 
 	for (i = 0; name[i] != '\0'; i++)
-		*namecopy = name[i];
+	{
+		namecopy[i] = name[i];
+
+	}
 
 	for (i = 0; owner[i] != '\0'; i++)
-		*ownercopy = owner[i];
+	{
+		ownercopy[i] = owner[i];
+	}
 
 	dog->name = namecopy;
 	dog->age = age;
