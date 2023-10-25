329e094c5313d134a435bc536473cd9529277877
Felizardo Massimbe <f2massimbe@gmail.com>
2023-10-25 15:43:07 -0700

    fix NULL handle

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * print_name - prints a name
  * @name: name to print
@@ -5,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	it (f == NULL)
+		exit(0);
 	f(name);
 }
