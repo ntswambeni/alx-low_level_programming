157df07b6ef803f436746d3b51da7713b5b5216c
Felizardo Massimbe <f2massimbe@gmail.com>
2023-10-24 20:06:15 -0700

    fix handle null pointer in attrs

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,11 +9,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name)
-			printf("Name: %s\n", d->name);
-		if (d->age)
-			printf("Age: %f\n", d->age);
-		if (d->owner)
-		printf("Owner: %s\n", d->owner);
+		printf("Name: %s\n", d->name ? d->name : "");
+		printf("Age: %f\n", d->age ? d->name : "");
+		printf("Owner: %s\n", d->owner ? d->name : "");
 	}
 }
