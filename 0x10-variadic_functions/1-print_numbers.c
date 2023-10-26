e26f19776340b6512643092cc58403490f3cfbab
Felizardo Massimbe <f2massimbe@gmail.com>
2023-10-25 18:25:12 -0700

    fix null handle

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (n == 0)
 		exit(0);
 	if (separator == NULL)
+	{
+		printf("");
 		separator = "";
+	}
 
 	va_start(list, n);
 	for (i = 0; i < (n - 1); i++)
