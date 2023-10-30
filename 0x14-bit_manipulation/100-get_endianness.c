946baa7449c5439bba1c95ecfc72d72ec2d88862
Felizardo Massimbe <f2massimbe@gmail.com>
2023-10-30 15:41:44 -0700

    add 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1U;
+	char *c =(char*)&num;
+
+	return ((int)*c);
+}
