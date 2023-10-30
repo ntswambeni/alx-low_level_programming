f6348765aee70b0f7bb8ba340bce01782d6b01cc
Felizardo Massimbe <f2massimbe@gmail.com>
2023-10-30 05:30:50 -0700

    add 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_bit - returns a bit at a given index
+ * @n: number to search
+ * @index: index of the bit
+ * Return: index, -1 if not found
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int num = 1UL;
+	unsigned long int size = (sizeof(unsigned long int) * 8);
+
+	if ((index > size) || n == NULL || index == NULL)
+		return (-1);
+	num = num << (index);
+	if (num & n)
+		return (1);
+	else
+		return (0);
+}
